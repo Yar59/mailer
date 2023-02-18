972e2c544a5eaeda60db9fc450ffa8f9ad821f36
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-02-18 11:47:40 +0300

    add Subscriber admin

--- mail_agent/admin.py
+++ mail_agent/admin.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subscriber
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    fields = (
+        'name',
+        'email',
+        'birthday',
+    )
