72018dce3e32817ce30fc9034a4ebb18df7afd32
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-02-18 13:30:42 +0300

    add delayed mailing

--- mail_agent/tasks.py
+++ mail_agent/tasks.py
@@ -1,7 +1,12 @@
+import smtplib
+
 from celery import shared_task
-from django.core.mail import send_mass_mail
+from django.core.mail import send_mail
 
 
-@shared_task
-def send_email_task(mails):
-    send_mass_mail(mails)
+@shared_task(bind=True, default_retry_delay=10 * 60)
+def send_email_task(self, subject, message, email_from, recipient_list, html_message=None):
+    try:
+        send_mail(subject, message, email_from, recipient_list, html_message=html_message)
+    except smtplib.SMTPException as ex:
+        self.retry(exc=ex, countdown=5)
--- mail_agent/views.py
+++ mail_agent/views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from datetime import datetime
+
+import pytz
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
+from django.utils import timezone
+from django.utils.html import strip_tags
 
 from models import Subscriber
 from .tasks import send_email_task
@@ -18,7 +23,8 @@ def create_mailing_list(request):
     if request.method == "POST":
         data = request.POST
         mailing_time = datetime.strptime(str((data.get("mailing-time"))), "%Y-%m-%dT%H:%M")
-        if mailing_time < datetime.now():
+        local_mailing_time = pytz.timezone("UTC").localize(mailing_time, is_dst=None)
+        if timezone.localtime(local_mailing_time) < timezone.localtime(timezone.now()):
             return JsonResponse(
                 data={
                     'status': 400,
@@ -26,7 +32,22 @@ def create_mailing_list(request):
                 },
                 status=400
             )
-        #TODO обработка рассылки
+        email_from = 'dyadkayar59@yandex.ru'
+        subscribers = Subscriber.objects.all()
+        for subscriber in subscribers:
+            html_message = render_to_string('../templates/message_template.html', {'context': 'values'})
+            plain_message = strip_tags(html_message)
+            send_email_task.apply_async(
+                (
+                'subject',
+                plain_message,
+                email_from,
+                [subscriber.email, ],
+                html_message,
+                ),
+                eta=local_mailing_time
+            )
+
         return JsonResponse(
             data={
                 'status': 201
@@ -41,14 +62,3 @@ def create_mailing_list(request):
             },
             status=404
         )
-    # email_from = 'dyadkayar59@yandex.ru'
-    # mails = []
-    # subscribers = Subscriber.objects.all()
-    # for subscriber in subscribers:
-    #     mails.append((
-    #         'subject',
-    #         'message',
-    #         email_from,
-    #         [subscriber.email, ],
-    #     ))
-    # send_email_task.delay(tuple(mails))
